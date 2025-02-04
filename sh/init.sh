2a0650116fe7fb179b0a523b3bc3298f9059a16d
尤雨东 <root@ltpp.vip>
2025-02-04 09:10:14 +0800

    feat: sh

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add ltpp ssh://git@git.ltpp.vip:66/root/std-macro-extensions.git;
+git remote add jihulab ssh://jihulab.com:ltpp/std-macro-extensions.git;
 git remote add github git@github.com:ltpp-universe/std-macro-extensions.git;
 git remote add origin git@github.com:ltpp-universe/std-macro-extensions.git;
