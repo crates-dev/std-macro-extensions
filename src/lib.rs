65969b549d50a6b4af9305d5ae07b66171e03d91
尤雨东 <root@ltpp.vip>
2026-01-21 12:20:33 +0800

    feat: v0.26.14

--- src/lib.rs
+++ src/lib.rs
@@ -4,29 +4,29 @@
 //! data structures, simplifying the creation and manipulation of
 //! common collections such as HashMap, Vec, and more.
 
-pub(crate) mod r#arc;
-pub(crate) mod b_tree_map;
-pub(crate) mod b_tree_set;
-pub(crate) mod binary_heap;
-pub(crate) mod boxed;
-pub(crate) mod r#cell;
-pub(crate) mod cin;
-pub(crate) mod cout;
-pub(crate) mod execute;
-pub(crate) mod hash_map;
-pub(crate) mod hash_set;
-pub(crate) mod linked_list;
-pub(crate) mod mutex;
-pub(crate) mod path;
-pub(crate) mod r#rc;
-pub(crate) mod ref_cell;
-pub(crate) mod rw_lock;
-pub(crate) mod string;
-pub(crate) mod vector;
-pub(crate) mod vector_deque;
+mod r#arc;
+mod b_tree_map;
+mod b_tree_set;
+mod binary_heap;
+mod boxed;
+mod r#cell;
+mod cin;
+mod cout;
+mod execute;
+mod hash_map;
+mod hash_set;
+mod linked_list;
+mod mutex;
+mod path;
+mod r#rc;
+mod ref_cell;
+mod rw_lock;
+mod string;
+mod vector;
+mod vector_deque;
 
 #[cfg(test)]
-pub(crate) use std::{
+use std::{
     boxed::Box,
     cell::{Cell, RefCell},
     collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque},
